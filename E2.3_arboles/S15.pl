3feb901638ab56d49aaff65311f9e9ca98a14d9b
itsODYSS3US <isaac.ascencio4173@alumnos.udg.mx>
2023-10-03 23:02:56 -0600

    E2.4 - Implementación de los árboles

--- E2.3_arboles/S15.pl
+++ E2.3_arboles/S15.pl
@@ -5,26 +5,27 @@
 %     arbol(e,i,o),
 %     u).
 
-arbol(a,
-    arbol(o,
-    arbol(e),
-    arbol(d)),
-    arbol(u)).
+arbol(a,arbol(o,arbol(e,arbol(s,a,c),arbol(c,n,i)),arbol(d,arbol(p,' ',a),arbol(l,i,l))),arbol(u,arbol(a,arbol(i,' ',s),arbol(c,a,' ')),arbol(e,arbol(i,l,s),arbol(' ',s,' ')))).
+arbol1(a,arbol1(s,c,e),arbol1(n,c,arbol1(i,o,' '))). 
+arbol2(o,arbol2(' ',arbol2(i,u,l),arbol2(s,s,e)),arbol2(i,arbol2(c,a,s),arbol2(c,e,n))).
 
 inorden(arbol(Raiz,Hizq,Hder)) :- 
     inorden(Hizq),
     write(Raiz),
     inorden(Hder), !.
 inorden(H) :- write(H).
+%inorden(arbol(a,arbol(o,arbol(e,arbol(s,a,c),arbol(c,n,i)),arbol(d,arbol(p,' ',a),arbol(l,i,l))),arbol(u,arbol(a,arbol(i,' ',s),arbol(c,a,' ')),arbol(e,arbol(i,l,s),arbol(' ',s,' '))))).
 
-preorden(arbol(Raiz,Hizq,Hder)) :-
+preorden(arbol1(Raiz,Hizq,Hder)) :-
     write(Raiz),
     preorden(Hizq),
     preorden(Hder), !.
 preorden(H) :- write(H).
+%preorden(arbol1(a,arbol1(s,c,e),arbol1(n,c,arbol1(i,o,' ')))).
 
-postorden(arbol(Raiz,Hizq,Hder)) :-
+postorden(arbol2(Raiz,Hizq,Hder)) :-
     postorden(Hizq),
     postorden(Hder),
     write(Raiz), !.
 postorden(H) :- write(H).
+%postorden(arbol2(o,arbol2(' ',arbol2(i,u,l),arbol2(s,s,e)),arbol2(i,arbol2(c,a,s),arbol2(c,e,n)))).
